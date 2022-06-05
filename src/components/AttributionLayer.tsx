00aee87272472dadafb3917c40edbfe7fe885ee4
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-05 11:18:13 +0100

    Tidy up the attribution layer

--- src/components/AttributionLayer.tsx
+++ src/components/AttributionLayer.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
+import { CLASS_NAMES } from '../constants'
+
 import { osmTileProvider } from './TileProviders'
 
+const classNames = {
+  attributionLayer: [CLASS_NAMES.primary, 'attribution-layer'].join(' '),
+}
+
+/**
+ * The prop type for the [[`AttributionLayer`]] component.
+ */
 export interface AttributionLayerProps {
   attribution?: React.ReactElement
 }
 
+/**
+ * Render the attribution layer.
+ */
 export default function AttributionLayer({
   attribution = osmTileProvider.attribution,
 }: AttributionLayerProps) {
   return (
     <div
-      className="jetblack-map attribution"
+      className={classNames.attributionLayer}
       style={{
         position: 'absolute',
         bottom: 0,
